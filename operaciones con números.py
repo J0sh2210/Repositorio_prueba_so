cd02b4420b7de8d565654e13f110ea064838dc6f
Josh <jramireza31@miumg.edu.gt>
2025-08-01 23:22:00 -0600

    Cambio a multiplicacion
    Se cambio la operacion a realizar por una multiplicacion

--- operaciones con números.py
+++ operaciones con números.py
@@ -1,5 +1,5 @@
-print("Ingrese dos números a sumar")
+print("Ingrese dos números a multiplicar")
 num1 = int(input("ingrese primer número "))
 num2 = int(input("ingrese segundo número "))
-result = num1 - num2
-print("La resta de los dos números es: ", result)
+result = num1 * num2
+print("La multiplicacion de los dos números es: ", result)
