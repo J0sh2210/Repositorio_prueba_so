c524870e6361081e1f286359f825c47d2a0575b5
Josh <jramireza31@miumg.edu.gt>
2025-08-01 23:19:57 -0600

    creacion archivo operaciones con numeros
    Se agrego un archivo que contiene la suma de dos números

---
+++ operaciones con números.py
@@ -0,0 +1,5 @@
+print("Ingrese dos números a sumar")
+num1 = int(input("ingrese primero número "))
+num2 = int(input("ingrese primero número "))
+result = num1 + num2
+print("La suma de los dos números es: ", result)
