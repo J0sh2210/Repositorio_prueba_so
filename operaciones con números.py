77b8b98d93f3c70caf5e44f4bff6d3ac7b2109ee
Josh <jramireza31@miumg.edu.gt>
2025-08-01 23:21:01 -0600

    Cambio en archivo operaciones con numeros
    Se realizó correcciones de escritura y el cambio de operacion a una resta

--- operaciones con números.py
+++ operaciones con números.py
@@ -1,5 +1,5 @@
 print("Ingrese dos números a sumar")
-num1 = int(input("ingrese primero número "))
-num2 = int(input("ingrese primero número "))
-result = num1 + num2
-print("La suma de los dos números es: ", result)
+num1 = int(input("ingrese primer número "))
+num2 = int(input("ingrese segundo número "))
+result = num1 - num2
+print("La resta de los dos números es: ", result)
